08ba895ad1d2f51da1905e4f16e3df6836b3f7d1
Kanakadandila Harsha <harshakanakadandila@gmail.com>
2026-04-07 21:05:20 +0530

    Input Validation

---
+++ backend/app/models/model.py
@@ -0,0 +1,36 @@
+#This code is the user input checker, only allows specific type of data to be sent to db.
+
+from pdantic import BaseModel, EmailStr # Used to enfore strict datatypes
+from typing import Optional
+from datetime import datetime
+
+
+class UserCreate(BaseModel):
+    name: str 
+    role: str 
+    email: EmailStr # EmailStr checks if the value is email or not
+
+class UserResponse(BaseModel):
+    id: int 
+    name: str 
+    role: str 
+    email: EmailStr
+    created_at: datetime # Return the timestamp of the request
+
+class FoodListingCreate(BaseModel):
+    title: str 
+    description: Optional[str]=None # This value is not compulsory, else None
+    qty: str 
+    provider_id: int
+
+class FoodListingResponse(BaseModel):
+    id: int
+    title: str 
+    description: Optional[str]=None
+    qty: str 
+    status: str 
+    provider_id: int
+    claimed-id: Optional[int]=None
+    created_at: datetime
+
+     
